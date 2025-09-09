27bba4f254092a6a88cbda864a3a5bda6802d0c9
Iván Lorenzo <elhibar77@hotmail.es>
2025-09-09 12:11:37 +0200

    Initial configuration

---
+++ src/main/java/f5/t4/clinica_veterinaria_back/ClinicaVeterinariaBackApplication.java
@@ -0,0 +1,13 @@
+package f5.t4.clinica_veterinaria_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaVeterinariaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaVeterinariaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f5/t4/clinica_veterinaria_back/ClinicaVeterinariaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package f5.t4.clinica_veterinaria_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaVeterinariaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
