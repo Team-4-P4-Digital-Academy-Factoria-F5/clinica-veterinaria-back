f5185c9cd3e1b608f578833c48cb046e15ad3477
Ivan Lorenzo <elhibar77@hotmail.es>
2025-09-10 15:05:06 +0200

    CM-9 feat: Role entity created

---
+++ src/main/java/f5/t4/clinica_veterinaria_back/role/RoleEntity.java
@@ -0,0 +1,20 @@
+package f5.t4.clinica_veterinaria_back.role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+}
