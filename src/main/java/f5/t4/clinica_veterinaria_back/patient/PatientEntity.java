81340f0465bf53f3f425f2e7a6b3d96987c7dea2
Yolanda <yolandaalfonsolopez@gmail.com>
2025-09-10 15:15:16 +0200

    CM-5 feat: PatientEntity created

---
+++ src/main/java/f5/t4/clinica_veterinaria_back/patient/PatientEntity.java
@@ -0,0 +1,33 @@
+package f5.t4.clinica_veterinaria_back.patient;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor 
+public class PatientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id_patient;
+
+    private String name;
+    private int age;
+    private String family;
+    private String breed;
+    private String sex;
+    private String dni_client;
+    
+}
