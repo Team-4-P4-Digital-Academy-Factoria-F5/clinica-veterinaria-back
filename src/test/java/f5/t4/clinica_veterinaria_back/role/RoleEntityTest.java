6c21e724abf19a2de66c0920d02b92bcd78fe88c
Iván Lorenzo <elhibar77@hotmail.es>
2025-09-11 13:56:42 +0200

    CM-9 feat: Role Entity test implemented

---
+++ src/test/java/f5/t4/clinica_veterinaria_back/role/RoleEntityTest.java
@@ -0,0 +1,27 @@
+package f5.t4.clinica_veterinaria_back.role;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class RoleEntityTest {
+
+    @Test
+    void testNoArgsConstructorAndSetters() {
+        RoleEntity role = new RoleEntity();
+        role.setId_role(1L);
+        role.setName("ROLE_ADMIN");
+
+        assertThat(role.getId_role(), is(1L));
+        assertThat(role.getName(), is(equalTo("ROLE_ADMIN")));
+    }
+
+    @Test
+    void testAllArgsConstructor() {
+        RoleEntity role = new RoleEntity(2L, "ROLE_CLIENT");
+
+        assertThat(role.getId_role(), is(2L));
+        assertThat(role.getName(), equalTo("ROLE_CLIENT"));
+    }
+}
