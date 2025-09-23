f150fb01b31915198699cf66ae7e9ba1287aa096
Yolanda <yolandaalfonsolopez@gmail.com>
2025-09-23 10:56:28 +0200

    CM-5 update:PatientRepository

--- src/main/java/f5/t4/clinica_veterinaria_back/patient/PatientRepository.java
+++ src/main/java/f5/t4/clinica_veterinaria_back/patient/PatientRepository.java
@@ -1,7 +1,13 @@
 package f5.t4.clinica_veterinaria_back.patient;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<PatientEntity, Long>{
+    List<PatientEntity> findByTutor(Long tutorId);
+    List<PatientEntity> findByName(String name);
+    Optional<PatientEntity> findByIdentificationNumber(String identification_number);
 
 }
